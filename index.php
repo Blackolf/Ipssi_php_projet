c36f8297e5e0bb98faf243cfd701662ee4a124d9
wu_leo <wu.leo.officia@gmail.com>
2019-11-03 15:13:42 +0100

    index modif

--- index.php
+++ index.php
@@ -1,32 +1,13 @@
 <?php
-namespace TP {
-  use TP\config\dbb ;
-  require_once ('main/main.php');
-  require_once ('config/routing.php');
-  require_once ('req/header.php');
-  require_once ('req/menu.php');
+require_once ('main/main.php');
+require_once ('config/routing.php');
+require_once ('req/header.php');
+require_once ('req/menu.php');
 
-
-
-
-  Dbb\test1();
-
-  // if(Dbb\connect()){
-  //   echo "connection réussi";
-  // }else {
-  //   echo "connection fail";
-  // }
-
-
-
-
-
-
-
-
-
-  // echo $context;
-
-
-  require_once ('req/footer.php');
+if(Dbb\connect()){
+  echo "connection réussi";
+}else {
+  echo "connection fail";
 }
+
+require_once ('req/footer.php');

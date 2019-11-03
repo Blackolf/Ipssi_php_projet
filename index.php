24c876e97f3eb47a46e15a5531617289bda59631
wu_leo <wu.leo.officia@gmail.com>
2019-11-03 17:27:49 +0100

    commit 1

--- index.php
+++ index.php
@@ -1,12 +1,25 @@
 <?php
 require_once ('main/main.php');
+require_once ('main/commun.fun.php');
 require_once ('config/routing.php');
 require_once ('req/header.php');
 
-if(Dbb\connect()){
-  echo "connection réussi";
-}else {
-  echo "connection fail";
+// if(Dbb\connect()){
+//   echo "connection réussi";
+// }else {
+//   echo "connection fail";
+// }
+$page = $_REQUEST['page'];
+
+
+switch ($page) {
+  case 'value':
+    // code...
+    break;
+
+  default:
+  getViews("loggin");
+    break;
 }
 
 require_once ('req/footer.php');

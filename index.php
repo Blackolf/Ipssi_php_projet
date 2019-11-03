423e33082abcf2535507085454b8251e072f002d
wu_leo <wu.leo.officia@gmail.com>
2019-11-03 16:55:11 +0100

    index modif

--- index.php
+++ index.php
@@ -1,10 +1,11 @@
 <?php
+require_once ('config/environnement.php');
 require_once ('main/main.php');
 require_once ('config/routing.php');
 require_once ('req/header.php');
 require_once ('req/menu.php');
 
-if(Dbb\connect()){
+if(connect()){
   echo "connection réussi";
 }else {
   echo "connection fail";
